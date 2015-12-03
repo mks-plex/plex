2a6d859ac709ab4fc4bb7642892e7d184c228d45
Matthew Murkidjanian <matthewmurkidjanian@gmail.com>
2015-12-03 13:38:58 -0800

    [feature] initialize react app component Establish react component file structure Write incomplete App component render method

---
+++ client/components/App.js
@@ -0,0 +1,20 @@
+/*
+  App
+*/
+
+var React = require('react');
+var Header = require('./Header');
+var Body = require('./Body');
+var Footer = require('./Footer');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    )
+  }
+});
