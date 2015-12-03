7be0411e641e07fe12574e0d9a59917499f3cc81
Jonathan Tamsut <jonathantamsut@Jonathans-MacBook-Pro-2.local>
2015-12-03 12:22:53 -0800

    [test] serve up static files for React testing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send('server up');
 });
+
+// serving up static html files for React.js testing
+
+app.use(express.static(__dirname + '/../client/static'));
 
 app.listen(port);
