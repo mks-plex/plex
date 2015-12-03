470f939106d1be664ce50ec2b18af8e49fde3bf5
Jonathan Tamsut <jonathantamsut@Jonathans-MacBook-Pro-2.local>
2015-12-03 15:07:12 -0800

    [feature] finish Header component

--- client/components/App.js
+++ client/components/App.js
@@ -18,3 +18,5 @@ var App = React.createClass({
     )
   }
 });
+
+//ReactDOM(<App />, document.getElementById("main"));
---
+++ client/components/Header.js
@@ -0,0 +1,13 @@
+/*
+  Header
+*/
+
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <h1>PLEX</h1>
+    )
+  }
+});
