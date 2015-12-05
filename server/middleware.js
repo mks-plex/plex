8946716675cfbe9b3928042d5c85d46753e56ad3
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2015-12-04 16:17:54 -0800

    [feature] modify middleware to handle request parameters

--- server/middleware.js
+++ server/middleware.js
@@ -2,9 +2,8 @@ var utils = require('./utilities.js');
 
 module.exports.evalForAllInputSizes = function(req, res, next) {
 	var userInput = req.body.userInput;
-	// var dataType = req.body.dataType
-	// TODO: pass dataType to evalAlg
-	var algorithmData = utils.evalAlg(userInput);
+	var dataType = req.params.dataType;
+	var algorithmData = utils.evalAlg(userInput, dataType);
 	var d3Coordinates = utils.getCoords(algorithmData);
 	res.coords = d3Coordinates;
 	next();
