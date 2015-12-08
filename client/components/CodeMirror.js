788089a0d785320425bf23370caf30b74b70d3c1
Jonathan Tamsut <jonathantamsut@Jonathans-MacBook-Pro-2.local>
2015-12-08 11:34:30 -0800

    [feature] add codemirror

--- client/components/CodeMirror.js
+++ client/components/CodeMirror.js
@@ -3,11 +3,31 @@
 */
 
 var React = require('react');
+var ReactCodeMirror = require('react-codemirror');
+require('codemirror/mode/javascript/javascript');
+require('../styles/codemirror.css');
 
 var CodeMirror = React.createClass({
+  getInitialState: function() {
+    return {
+      code: 'Write your algorithm here.',
+      mode: 'javascript'
+    }
+  },
+  updateCode: function(newCode) {
+    this.setState({
+      code: newCode
+    })
+  },
   render: function() {
+    var options = {
+      lineNumbers: true,
+      mode: 'javascript',
+      tabSize: 2,
+    };
     return (
       <div className="codeMirror-container">
+          <ReactCodeMirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} />
       </div>
     )
   }
