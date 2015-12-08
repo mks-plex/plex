0200bfd55a60affdf80c3c914fd56a725da6bfe8
Stephanie Raad <stephy.raad@gmail.com>
2015-12-08 11:53:46 -0800

    [refactor] write db queries and create integer controller Add schema and model to integer object Write queries to fetch data from db

---
+++ server/queries.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var integerController = require('./routes/integerController');
+
+// connect to the database 
+mongoose.connect('mongodb://localhost/local');
+
+// grab the connection object from mongoose
+var db = mongoose.connection;
+
+// watch for changes
+db.on('error', function(msg) {
+  console.log("db connection failed");
+});
+
+db.once('open', function() {
+  console.log("db connection succeeded");
+});
+
+exports.getData = function() {
+  // if(datatype === "integers"){
+    return integerController.getIntegers();
+  // }
+}
+  
