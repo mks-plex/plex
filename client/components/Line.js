8a275260152596c94367c1c82539f9cd3477fde8
Matthew Murkidjanian <matthewmurkidjanian@gmail.com>
2015-12-09 17:07:03 -0800

    [chore] resolve merge conflict in utilities

---
+++ client/components/Line.js
@@ -0,0 +1,23 @@
+/*
+  Line
+*/
+
+var React = require('react');
+
+var Line = React.createClass({
+  getDefaultProps: function() {
+    return {
+      path: '',
+      color: 'blue',
+      width: 2
+    }
+  },
+
+  render: function() {
+    return (
+      <path d={this.props.path} stroke={this.props.color} strokeWidth={this.props.width} fill="none" />
+    );
+  }
+});
+
+module.exports = Line;
