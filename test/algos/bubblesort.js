473b2b4d701374197ebddf836c8cb6d5558a0427
Jonathan Tamsut <jonathantamsut@Jonathans-MacBook-Pro-2.local>
2015-12-14 15:23:40 -0800

    [test] create testing suite

--- test/algos/bubblesort.js
+++ test/algos/bubblesort.js
@@ -1,6 +1,6 @@
 // Worst case O(n^2)
 
-function bubbleSort(array) {
+module.exports = function bubbleSort(array) {
   var length = array.length;
   for (var i = 0; i < length; i++) {
     for (var j = 0; j < (length - i - 1); j++) {
@@ -13,5 +13,3 @@ function bubbleSort(array) {
   }
   return array;
 };
-
-console.log(bubbleSort([6,7,3,4,5,6,2,3,4,5,6,71,2,3,54,3,2,3,43,7]));
