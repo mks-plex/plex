7237a3fad4b03912324c50ad8abc135c29372f4d
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2015-12-16 14:22:05 -0800

    [feature] add equation and function name to response object

--- server/middleware.js
+++ server/middleware.js
@@ -1,18 +1,24 @@
 var Promise = require('bluebird');
+var eval = require('./evaluation.js');
 var utils = require('./utilities.js');
 
 module.exports.evalForAllInputSizes = function(req, res, next) {
   var userInput = req.body.data;
   var dataType = req.params.dataType || null; 
 
-  console.log('M8-string received from ajax: ' + userInput);
-
   return new Promise(function(resolve, reject) {
-    var data = utils.evalAlg(userInput, dataType);
+    var data = eval.evalAlg(userInput, dataType);
     resolve(data);
   }).then(function(data) {
-    var coords = utils.getJSONCoords(data);
-    res.coords = coords;
+    res.body = {};
+
+    // TODO: integrate jon's bigO module
+    res.body.bigO = null;
+
+    res.body.name = utils.getFuncName(userInput);
+    res.body.eq = eval.runRegression(data, null);
+    var coords = eval.getJSONCoords(data);
+    res.body.coords = coords;
     next();
   });
 };
