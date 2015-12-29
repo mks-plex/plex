c185cac0b40fac6f38774eb3e34f681668146abc
Stephanie Raad <stephy.raad@gmail.com>
2015-12-28 19:10:28 -0800

    [feature] Error message modal

--- client/components/Body.js
+++ client/components/Body.js
@@ -6,12 +6,19 @@ var React = require('react');
 var $ = require('jquery');
 var CodeMirror = require('./CodeMirror');
 var Analysis = require('./Analysis');
+var Modal = require('react-modal');
+
 
 var Body = React.createClass({
   getInitialState: function() {
     return {
-      data: []
+      data: [],
+      modalIsOpen: false
     };
+  },
+
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
   },
 
   getCode: function(newCode) {
@@ -34,6 +41,8 @@ var Body = React.createClass({
       }.bind(this),
       error: function(err) {
         console.error('Oups! You have a ' + err.status + ' error.' + err.responseText);
+        this.setState({modalIsOpen: true});
+        console.log("SHOUDL ", this.state.modalIsOpen);
       }.bind(this)
     });
   },
@@ -49,12 +58,45 @@ var Body = React.createClass({
         width: '100%'
       }
     };
+    var styleModal ={
+      overlay : {
+        position          : 'fixed',
+        top               : 0,
+        left              : 0,
+        right             : 0,
+        bottom            : 0,
+        backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+      },
+      content : {
+        position                   : 'absolute',
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+        border                     : '1px solid #ccc',
+        background                 : '#fff',
+        overflow                   : 'auto',
+        WebkitOverflowScrolling    : 'touch',
+        borderRadius               : '4px',
+        outline                    : 'none',
+        padding                    : '20px'  
+      }
+    }
 
     return (
       <div className="main-content" style={style.content}>
         <p id="intro-message" style={style.intro}>{ this.props.intro }</p>
         <CodeMirror getCode={this.getCode} />
         <Analysis data={this.state.data} />
+        { this.state.modalIsOpen ? 
+          <div>
+            <Modal isOpen={this.state.modalIsOpen}onRequestClose={this.closeModal} style={styleModal} >
+              <p>Error! you're function doesn't sort</p>
+              <button onClick={this.closeModal}>Close</button>
+            </Modal>
+          </div> : <span /> }
       </div>
     )
   }
