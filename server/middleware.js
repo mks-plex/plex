4b77a10f046c73de8820d5929ed846c0881ed826
Stephanie Raad <stephy.raad@gmail.com>
2015-12-30 14:37:56 -0800

    [feature] User func error handling

--- server/middleware.js
+++ server/middleware.js
@@ -24,16 +24,44 @@ module.exports.evalForAllInputSizes = function(req, res, next) {
   });
 };
 
-module.exports.testAlgo = function(req, res, next) {
-  var userInput = req.body.data;
-  var userAlg = utils.memoBuild(userInput);
-  var testArray = [10,9,8,7,6,5,4,3,2,1];
-  var ordArray = [1,2,3,4,5,6,7,8,9,10];
-  var result = userAlg(testArray);
+// module.exports.testAlgo = function(req, res, next) {
+//   var userInput = req.body.data;
+//   var testArray = [10,9,8,7,6,5,4,3,2,1];
+//   var ordArray = [1,2,3,4,5,6,7,8,9,10];
 
-  if(result.join() === ordArray.join()) {
-    next();
-  } else {
-    res.status(200).send("Error! You're function doesn't sort.");
-  }
-};
+//   // Test if UserInput can be built
+//   try {
+//     console.log('Try One');
+//     var param = userInput.slice(userInput.indexOf('(') + 1, userInput.indexOf(')'));
+//     var algString = userInput.slice(userInput.indexOf('{') + 1, userInput.lastIndexOf('}'));
+//     var userAlg = new Function(param, algString);
+//     if (!algString || !param) res.status(200).send("Error! No param or function body.");
+//   } catch(e) {
+//     if (e instanceof SyntaxError) {
+//       console.log('caught Syntax error, couldn\'t make function');
+//       console.log(e);
+//       res.status(200).send("Error! Your code is not a function");
+//     }
+//     return;
+//   }
+//   // Test if Built function can be
+//   try {
+//     console.log('try2');
+//     var userAlg = utils.memoBuild(userInput);
+//     userAlg(testArray)
+//   } catch(e) {
+//     if (e instanceof SyntaxError) { 
+//       console.log('caught Syntax error, function didn\'t run');
+//       console.log(e);
+//       res.status(200).send("Error! That is not an executable function.")
+//     }
+//     return;
+//   }
+//   var result = userAlg(testArray);
+//   if (result.join() === ordArray.join()) {
+//     console.log('alg passed');
+//     next();
+//   } else {
+//     res.status(200).send("Error! Your function doesn't sort.");
+//   }
+// };
