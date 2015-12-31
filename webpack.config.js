d74880fe49e1ae97e496250b3fb00911dad03816
Matthew Murkidjanian <matthewmurkidjanian@gmail.com>
2015-12-31 10:43:10 -0800

    [feature] add minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './client/main.js',
   output: {
     path: __dirname + '/client/build',
     filename: 'bundle.js',
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: true,
+      compress: {
+        warnings: false
+      },
+      minimize: true
+    }),
+  ],
   module: {
     loaders: [
       {
