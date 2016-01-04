b56ada97cf8ec0b8dc9bbc96908c6787bf91571c
Matthew Murkidjanian <matthewmurkidjanian@gmail.com>
2016-01-04 15:02:14 -0800

    [chore] remove the now ignored "test" directory

--- test/algos/linear.js
+++
@@ -1,9 +0,0 @@
-function linear(array) {
-  for (var index=0; index<array.length; index++) {
-    if (array[index] === true) {
-      console.log('Hey whats up');
-    }
-  }
-}
-
-module.exports = linear;
